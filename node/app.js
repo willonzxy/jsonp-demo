7a624beee4748372f524429791895db0fc2478b3
willon <1061258787@qq.com>
2018-09-09 21:14:36 +0800

    add node server code

---
+++ node/app.js
@@ -0,0 +1,19 @@
+﻿/*
+ * @Author: 伟龙-Willon qq:1061258787 
+ * @Date: 2018-09-09 21:05:09 
+ * @Last Modified by:   伟龙-Willon 
+ * @Last Modified time: 2018-09-09 21:05:09 
+ */
+
+var express = require('express');
+var app = express();
+
+var data = {name:'willon'};
+
+app.get('/jsonp',function(req,res,next){
+    var cb = req.query.cb;
+    console.log('回调函数的名称-->'+cb+'\n');
+    res.send(cb+'('+JSON.stringify(data)+')');
+});
+
+app.listen(3001);
